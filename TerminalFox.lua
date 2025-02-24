fa1aae90ea320aef85b1777fd1569e6d6ca88e45
TheLuaFox86 <peanuts005@outlook.com>
2025-02-24 18:58:50 -0500

    Update TerminalFox.lua

--- TerminalFox.lua
+++ TerminalFox.lua
@@ -1,6 +1,6 @@
 local player, gui
-local a = instance.new("Frame")
-local b = instance.new("TextBox")
+local a = Instance.new("Frame")
+local b = Instance.new("TextBox")
 b.Text = "16"
 b.FocusLost:Connect(function(EnterPressed)
     if EnterPressed then
