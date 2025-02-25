52debf8e9db081bdf0ae0e1ddc4b08a97032c3ef
TheLuaFox86 <peanuts005@outlook.com>
2025-02-24 20:30:45 -0500

    Update TerminalFox.lua

--- TerminalFox.lua
+++ TerminalFox.lua
@@ -2,10 +2,5 @@ local player, gui = ...
 local a = Instance.new("Frame")
 local b = Instance.new("TextBox")
 b.Text = "16"
-b.FocusLost:Connect(function(EnterPressed)
-    if EnterPressed then
-      player.Character.Humanoid.WalkSpeed = tonumber(b.Text)
-    end
-end
 a.Parent = gui.body
 b.Parent = a
