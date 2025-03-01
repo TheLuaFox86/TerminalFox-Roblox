dd685e2b13643da24fd497d09a0b259979dafa12
TheLuaFox86 <peanuts005@outlook.com>
2025-02-28 21:24:37 -0500

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -1,3 +1,14 @@
 local cmds = {}
-
+cmds.dex = function(tf, p, m, pv)
+  local a = game
+  for _,o in ipairs(p:split(.)) do
+    a = a[o]
+  end  
+  for v in pairs(a:GetChildren()) do
+    tf.print(v.name)
+  end
+  for _pv in pairs(a) do
+    tf.print(tostring(_pv))
+  end
+end
 return cmds
